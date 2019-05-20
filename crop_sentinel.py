06af3047006b418032d85ea8afd21ad7b5951f12
Sara Mirzaee <sxm1611@login4.pegasus.edu>
2019-05-20 07:26:21 -0400

    use memmap to read array

--- crop_sentinel.py
+++ crop_sentinel.py
@@ -14,12 +14,13 @@ import time
 import glob
 from isceobj.Util.ImageUtil import ImageLib as IML
 from mergeBursts import multilook
-from rinsar.utils.process_utilities import create_or_update_template
+from minsar.utils.process_utilities import create_or_update_template, set_dask_memory_walltime
 from pysqsar_utilities import convert_geo2image_coord, patch_slice
-from rinsar.objects.auto_defaults import PathFind
+from minsar.objects.auto_defaults import PathFind
 
 pathObj = PathFind()
 ##############################################################################
+
 
 def create_parser():
     """ Creates command line argument parser object. """
@@ -40,22 +41,20 @@ def command_line_parse(iargs=None):
     return inps
 
 
-def cropSLC(inpstring):
+def cropSLC(data):
+    '''crop SLC images'''
 
-    splitstr = inpstring.split()
-    input_file = splitstr[splitstr.index('--input') + 1]
-    output_file = splitstr[splitstr.index('--output') + 1]
-    print(input_file)
-    print(output_file)
+    (input_file, output_file) = data
+
     ds = gdal.Open(input_file + '.vrt', gdal.GA_ReadOnly)
-    inp_file = ds.GetRasterBand(1).ReadAsArray()
-    inp_file = inp_file[pathObj.first_row:pathObj.last_row, pathObj.first_col:pathObj.last_col]
+    inp_file = ds.GetVirtualMemArray()
+    data_type = inp_file.dtype.type
     del ds
 
-    out_map = np.memmap(output_file, dtype=np.complex64, mode='write', shape=(pathObj.n_lines, pathObj.width))
-    out_map[:, :] = inp_file
+    out_map = np.memmap(output_file, dtype=data_type, mode='write', shape=(pathObj.n_lines, pathObj.width))
+    out_map[:, :] = inp_file[pathObj.first_row:pathObj.last_row, pathObj.first_col:pathObj.last_col]
 
-    IML.renderISCEXML(output_file, 1, pathObj.n_lines, pathObj.width, IML.NUMPY_type('complex64'), 'BIL')
+    IML.renderISCEXML(output_file, 1, pathObj.n_lines, pathObj.width, IML.NUMPY_type(str(inp_file.dtype)), 'BIL')
 
     out_img = isceobj.createSlcImage()
     out_img.load(output_file + '.xml')
@@ -65,11 +64,10 @@ def cropSLC(inpstring):
     return output_file
 
 
-def cropQualitymap(inpstring):
+def cropQualitymap(data):
+    '''crop geometry files: lat, lon, ...'''
 
-    splitstr = inpstring.split()
-    input_file = splitstr[splitstr.index('--input') + 1]
-    output_file = splitstr[splitstr.index('--output') + 1]
+    (input_file, output_file) = data
 
     img = isceobj.createImage()
     img.load(input_file + '.xml')
@@ -78,48 +76,44 @@ def cropQualitymap(inpstring):
     scheme = img.scheme
 
     ds = gdal.Open(input_file + '.vrt', gdal.GA_ReadOnly)
-    inp_file = ds.GetRasterBand(1).ReadAsArray()
-    inp_file = inp_file[pathObj.first_row:pathObj.last_row, pathObj.first_col:pathObj.last_col]
+    inp_file = ds.GetVirtualMemArray()
+    del ds
+
+    out_map = IML.memmap(output_file, mode='write', nchannels=bands,
+                         nxx=pathObj.width, nyy=pathObj.n_lines, scheme=scheme, dataType=data_type)
 
     if bands == 2:
-        inp_file2 = ds.GetRasterBand(2).ReadAsArray()
-        inp_file2 = inp_file2[pathObj.first_row:pathObj.last_row, pathObj.first_col:pathObj.last_col]
-    del ds, img
+        out_map.bands[0][:, :] = inp_file[0][pathObj.first_row:pathObj.last_row, pathObj.first_col:pathObj.last_col]
+        out_map.bands[1][:, :] = inp_file[1][pathObj.first_row:pathObj.last_row, pathObj.first_col:pathObj.last_col]
+    else:
+        out_map.bands[0][:, :] = inp_file[0][pathObj.first_row:pathObj.last_row, pathObj.first_col:pathObj.last_col]
 
-    if not (inp_file.shape[0] == pathObj.n_lines and inp_file.shape[1] == pathObj.width):
+    IML.renderISCEXML(output_file, bands,
+                      pathObj.n_lines, pathObj.width,
+                      data_type, scheme)
 
-        out_map = IML.memmap(output_file, mode='write', nchannels=bands,
-                             nxx=pathObj.width, nyy=pathObj.n_lines, scheme=scheme, dataType=data_type)
+    out_img = isceobj.createImage()
+    out_img.load(output_file + '.xml')
+    out_img.renderHdr()
+    out_img.renderVRT()
+    try:
+        out_map.bands[0].base.base.flush()
+    except:
+        pass
 
-        if bands == 2:
-            out_map.bands[0][0::, 0::] = inp_file
-            out_map.bands[1][0::, 0::] = inp_file2
-        else:
-            out_map.bands[0][0::, 0::] = inp_file
-
-        IML.renderISCEXML(output_file, bands,
-                          pathObj.n_lines, pathObj.width,
-                          data_type, scheme)
-
-        out_img = isceobj.createImage()
-        out_img.load(output_file + '.xml')
-        out_img.imageType = data_type
-        out_img.renderHdr()
-        out_img.renderVRT()
-        try:
-            out_map.bands[0].base.base.flush()
-        except:
-            pass
-
-        del out_map
+    del out_map
 
     return output_file
 
 
-def main(inps):
-    """
-    Crops SLC images from Isce merged/SLC directory.
-    """
+if __name__ == '__main__':
+    '''
+    Crop SLCs and geometry.
+    '''
+
+    inps = command_line_parse()
+    inps = create_or_update_template(inps)
+    inps.geo_master = os.path.join(inps.work_dir, pathObj.geomasterdir)
 
     slc_list = os.listdir(os.path.join(inps.work_dir, pathObj.mergedslcdir))
     slc_list = [os.path.join(inps.work_dir, pathObj.mergedslcdir, x, x + '.slc.full') for x in slc_list]
@@ -146,72 +140,14 @@ def main(inps):
     run_list_geo = []
 
     for item in slc_list:
-        line = '--input {i} --output {o}'.format(i=item, o=item.split('.full')[0])
-        run_list_slc.append(line)
-
+        run_list_slc.append((item, item.split('.full')[0]))
 
     for item in meta_data:
-        line = '--input {i} --output {o}'.format(i=os.path.join(inps.geo_master, item + '.rdr.full'),
-                                                 o=os.path.join(inps.geo_master, item + '.rdr'))
-        run_list_geo.append(line)
+        run_list_geo.append((os.path.join(inps.geo_master, item + '.rdr.full'),
+                                  os.path.join(inps.geo_master, item + '.rdr')))
 
-    futures = []
-    start_time = time.time()
     for item in run_list_slc:
-        future = client.submit(cropSLC, item, retries=3)
-        futures.append(future)
+        cropSLC(item)
 
     for item in run_list_geo:
-        future = client.submit(cropQualitymap, item, retries=3)
-        futures.append(future)
-
-    i_future = 0
-    for future, result in as_completed(futures, with_results=True):
-        i_future += 1
-        print("FUTURE #" + str(i_future), "complete in", time.time() - start_time, "seconds.")
-
-    cluster.close()
-    client.close()
-
-    return
-
-
-if __name__ == '__main__':
-    '''
-    Crop SLCs.
-    '''
-
-    inps = command_line_parse()
-    inps = create_or_update_template(inps)
-    inps.geo_master = os.path.join(inps.work_dir, pathObj.geomasterdir)
-
-    try:
-        from dask.distributed import Client, as_completed
-        # dask_jobqueue is needed for HPC.
-        # PBSCluster (similar to LSFCluster) should also work out of the box
-        from dask_jobqueue import LSFCluster
-        python_executable_location = sys.executable
-        # Look at the ~/.config/dask/dask_pysar.yaml file for Changing the Dask configuration defaults
-        cluster = LSFCluster(config_name='rsmas_insar',
-                             python=python_executable_location,
-                             memory=inps.rinsar_dask_memory,
-                             walltime=inps.rinsar_dask_walltime)
-        NUM_WORKERS = int(inps.rinsar_dask_num_workers)
-        cluster.scale(NUM_WORKERS)
-
-        print("JOB FILE:", cluster.job_script())
-        # This line needs to be in a function or in a `if __name__ == "__main__":` block. If it is in no function
-        # or "main" block, each worker will try to create its own client (which is bad) when loading the module
-        client = Client(cluster)
-
-    except:
-        print('parallel not possible')
-        #from distributed import Client, LocalCluster
-
-        #cluster = LocalCluster(config_name='rsmas_insar',
-        #                       python=python_executable_location,
-        #                       memory=inps.rinsar_dask_memory,
-        #                       walltime=inps.rinsar_dask_walltime)
-        #client = Client(cluster)
-
-    main(inps)
+        cropQualitymap(item)
